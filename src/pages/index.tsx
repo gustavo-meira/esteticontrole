4db023a62164c922c37392b74d9dc0adb451b812
Gustavo Meira <gmeira.antonio@gmail.com>
2023-01-09 16:43:44 -0300

    feat: create a button to redirect to create client page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 const Home = () => (
   <>
     <Head>
       <title>Página Inicial - Esteticontrole</title>
     </Head>
+    <Link href="/create-client">
+      <button type="button">
+        Criar Cliente
+      </button>
+    </Link>
   </>
 );
 
