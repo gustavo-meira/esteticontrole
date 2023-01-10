6aee81d50de930d9baa4488ee96e31e4c137ca85
Gustavo Meira <gmeira.antonio@gmail.com>
2023-01-09 22:05:06 -0300

    feat: now the client route accepts post method to create a client

--- src/pages/api/client/index.ts
+++ src/pages/api/client/index.ts
@@ -1,16 +1,75 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { z } from 'zod';
 import { prisma } from '../../../lib/prisma';
 
-const acceptedMethods = ['GET'];
+const acceptedMethods = ['GET', 'POST'];
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (!acceptedMethods.includes(req.method as string)) {
-    res.status(505).json({ message: 'Method not implemented.' });
+    res.status(405).json({ message: 'Method not implemented.' });
   }
 
-  const clients = await prisma.client.findMany({});
+  if (req.method === 'GET') {
+    const clients = await prisma.client.findMany({});
+  
+    res.status(200).json({ clients });
+  }
 
-  res.status(200).json({ clients });
+  if (req.method === 'POST') {
+    const clientToCreateSchema = z.object({
+      name: z.string(),
+      birthDate: z.string()
+        .datetime()
+        .refine((birthDate) => new Date(birthDate)),
+      drink: z.boolean(),
+      smoke: z.boolean(),
+      children: z.number().optional(),
+      sleep: z.enum(['Bom', 'Ruim', 'Regular']),
+      feeding: z.enum(['Bom', 'Ruim', 'Regular']),
+      drinkWater: z.number(),
+      intestine: z.enum(['Bom', 'Ruim', 'Regular']),
+      surgeries: z.string().optional(),
+      illnesses: z.string().optional(),
+      medicines: z.string().optional(),
+      illnessesInFamily: z.string().optional(),
+      mentalHealth: z.string().optional(),
+      otherTreatments: z.string().optional(),
+      indication: z.string().optional(),
+      startingWeight: z.number().optional(),
+      measures: z.object({
+        rightArm: z.number().optional(),
+        leftArm: z.number().optional(),
+        chest: z.number().optional(),
+        waist: z.number().optional(),
+        hips: z.number().optional(),
+        butt: z.number().optional(),
+        rightThigh: z.number().optional(),
+        leftThigh: z.number().optional(),
+        rightCalf: z.number().optional(),
+        leftCalf: z.number().optional(),
+        height: z.number().optional(),
+      }).optional(),
+    });
+
+    try {
+      const dataParsed = clientToCreateSchema.parse(req.body);
+      const clientCreated = await prisma.client.create({
+        data: {
+          ...dataParsed,
+          measures: {
+            create: dataParsed.measures,
+          },
+        },
+        include: {
+          measures: true,
+        },
+      });
+  
+      res.status(201).json(clientCreated);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  }
 };
 
 export default handler;
