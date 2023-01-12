d464fd2c52af8b20f2b6e79a633e6fc30b604ab6
Gustavo Meira <gmeira.antonio@gmail.com>
2023-01-12 10:50:08 -0300

    feat: create a page to get the informations about one client

---
+++ src/pages/client/[id].tsx
@@ -0,0 +1,40 @@
+import { Client, Measures } from '@prisma/client';
+import { GetServerSideProps } from 'next';
+import { api } from '../../lib/api';
+
+type ClientComplete = Client & { measures?: Measures };
+
+type ClientPageProps = {
+  client: ClientComplete | null,
+};
+
+const ClientPage = ({ client }: ClientPageProps) => {
+  
+  if (!client) return <p>Client not found.</p>;
+  
+  return (
+    <p>{JSON.stringify(client, null, 2)}</p>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const host = process.env.HOST as string;
+  try {
+    const client = await api.get<ClientComplete>(`${host}/api/client/${ctx.query.id}`);
+  
+    return {
+      props: {
+        client: client.data,
+      },
+      
+    };
+  } catch {
+    return {
+      props: {
+        client: null,
+      },
+    };
+  }
+};
+
+export default ClientPage;
