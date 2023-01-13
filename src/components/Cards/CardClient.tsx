e492a162a732eedb76404159df0cb0d649151aeb
Gustavo Meira <gmeira.antonio@gmail.com>
2023-01-13 16:14:13 -0300

    fix: client birth date is now a state to fix the hydration

--- src/components/Cards/CardClient.tsx
+++ src/components/Cards/CardClient.tsx
@@ -1,18 +1,25 @@
 import { Client } from '@prisma/client';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 type CardClientProps = {
   client: Client;
 };
 
 export const CardClient = ({ client }: CardClientProps) => {
-  const clientBirthDate = new Date(client.birthDate);
+  const [clientBirthDate, setClientBirthDate] = useState<Date | null>(null);
+
+  useEffect(() => {
+    if (!clientBirthDate) {
+      setClientBirthDate(new Date(client.birthDate));
+    }
+  }, []);
 
   return (
     <Link href={`/client/${client.id}`}>
       <div>
         <h3>{client.name}</h3>
-        <p>{clientBirthDate.toLocaleDateString()}</p>
+        <p>{clientBirthDate?.toLocaleDateString()}</p>
         <p>{client.description}</p>
       </div>
     </Link>
