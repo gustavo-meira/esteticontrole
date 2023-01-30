22a635938d291206bead2d7786a7a8f34947b28c
Gustavo Meira <gmeira.antonio@gmail.com>
2023-01-30 13:59:28 -0300

    feat: package id route now accepts PATCH method to change the paid status

--- src/pages/api/package/[id].ts
+++ src/pages/api/package/[id].ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { z, ZodError } from 'zod';
 import { prisma } from '../../../lib/prisma';
 
-const acceptedMethods = ['PUT'];
+const acceptedMethods = ['PUT', 'PATCH'];
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (!acceptedMethods.includes(req.method as string)) {
@@ -31,6 +31,36 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         data: packageToEdit,
       });
   
+      res.status(200).json(packageEdited);
+    } catch (err) {
+      if (err instanceof ZodError) {
+        res.status(400).json({ message: err.message });
+      }
+
+      res.status(500).send({});
+    }
+  }
+
+  if (req.method === 'PATCH') {
+    const id = req.query.id;
+
+    const packageToEditSchema = z.object({
+      id: z.string().cuid(),
+      paid: z.boolean(),
+    });
+
+    try {
+      const packageToEdit = packageToEditSchema.parse({ ...req.body, id });
+
+      const packageEdited = await prisma.package.update({
+        where: {
+          id: packageToEdit.id,
+        },
+        data: {
+          paid: packageToEdit.paid,
+        },
+      });
+
       res.status(200).json(packageEdited);
     } catch (err) {
       if (err instanceof ZodError) {
