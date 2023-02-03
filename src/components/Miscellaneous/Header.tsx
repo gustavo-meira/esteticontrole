234d2649bea5714d085f24d8647e805bae68b3a1
Gustavo Meira <gmeira.antonio@gmail.com>
2023-02-03 15:49:48 -0300

    style: styling the header component

--- src/components/Miscellaneous/Header.tsx
+++ src/components/Miscellaneous/Header.tsx
@@ -1,26 +1,68 @@
-import Image from 'next/image';
 import Link from 'next/link';
+import { Flex, Image, ListItem, Text, UnorderedList } from '@chakra-ui/react';
+
+type TextLinkProps = {
+  href: string;
+  text: string;
+};
+
+const TextLink = ({href, text}: TextLinkProps) => (
+  <Link href={href}>
+    <Text
+      fontFamily="Poppins"
+      fontSize="3xl"
+      color="#00000099"
+      _hover={{
+        textDecor: 'underline',
+      }}
+    >
+      {text}
+    </Text>
+  </Link>
+);
 
 export const Header = () => (
-  <header>
-    <div>
-      <Link href="/">
+  <Flex
+    as="header"
+    backgroundColor="#F1D7FF96"
+    height="32"
+    pl="7"
+    pr="7"
+    justifyContent="space-between"
+  >
+    <Link href="/">
+      <Flex
+        gap="7"
+        alignItems="center"
+        height="full"
+      >
         <Image
           src="/assets/lotus-flower.svg"
           alt="Flor de lótus"
           width={98}
           height={98}
-          priority
         />
-      </Link>
-      <h2>EstetiControle</h2>
-    </div>
+        <Text
+          fontFamily="Parisienne"
+          fontSize="6xl"
+          color="black"
+        >
+        EstetiControle
+        </Text>
+      </Flex>
+    </Link>
     <nav>
-      <ul>
-        <li><Link href="/schedule">Agenda</Link></li>
-        <li><Link href="/client">Cliente</Link></li>
-        <li><Link href="/statistics">Estatísticas</Link></li>
-      </ul>
+      <UnorderedList
+        display="flex"
+        gap="5"
+        alignItems="center"
+        height="full"
+        listStyleType="none"
+      >
+        <ListItem><TextLink href="/schedule" text="Agenda" /></ListItem>
+        <ListItem><TextLink href="/client" text="Cliente" /></ListItem>
+        <ListItem><TextLink href="/statistics" text="Estatísticas" /></ListItem>
+      </UnorderedList>
     </nav>
-  </header>
+  </Flex>
 );
