22b683e1f745d0b878490dae23785223a9174ec2
Gustavo Meira <gmeira.antonio@gmail.com>
2023-02-03 21:02:47 -0300

    fix<IndexPage>: Removing button to create a new client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import { Header } from '../components/Miscellaneous/Header';
 
 const Home = () => (
@@ -8,11 +7,6 @@ const Home = () => (
       <title>Página Inicial - Esteticontrole</title>
     </Head>
     <Header />
-    <Link href="/create-client">
-      <button type="button">
-        Criar Cliente
-      </button>
-    </Link>
   </>
 );
 
