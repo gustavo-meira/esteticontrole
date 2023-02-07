799bd8e2836df9e760bd086426c77992e5ff3265
Gustavo Meira <gmeira.antonio@gmail.com>
2023-02-07 13:02:51 -0300

    build: create a container basic component

---
+++ src/components/Containers/ContainerBasic.tsx
@@ -0,0 +1,19 @@
+import { Container } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+type ContainerBasicProps = {
+  children: ReactNode;
+};
+
+export const ContainerBasic = ({ children }: ContainerBasicProps) => (
+  <Container
+    maxWidth="full"
+    minHeight="100vh"
+    display="flex"
+    flexDir="column"
+    bgColor="#FBFBFB"
+    p="0"
+  >
+    {children}
+  </Container>
+);
