d167772ce90789151763d159259f7b8eaa286563
Gustavo Meira <gmeira.antonio@gmail.com>
2023-02-08 13:28:37 -0300

    feat: mounting the page to edit the client

--- src/pages/client/[id]/edit.tsx
+++ src/pages/client/[id]/edit.tsx
@@ -1,5 +1,32 @@
-const ClientEditPage = () => (
-  <p>Editar cliente</p>
+import { Client, Measures } from '@prisma/client';
+import { GetServerSideProps } from 'next';
+import { ContainerBasic } from '../../../components/Containers/ContainerBasic';
+import { FormsCreateClient } from '../../../components/Forms/FormsCreateClient';
+import { Header } from '../../../components/Miscellaneous/Header';
+import { api } from '../../../lib/api';
+
+type ClientEditPageProps = {
+  client: Client & { measures: Measures };
+};
+
+const ClientEditPage = ({ client }: ClientEditPageProps) => (
+  <ContainerBasic>
+    <Header />
+    <FormsCreateClient client={client} />
+  </ContainerBasic>
 );
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const id = ctx.query.id as string;
+  const host = process.env.HOST as string;
+
+  const client = await api.get<Client & { measures: Measures }>(`${host}/api/client/${id}`);
+
+  return {
+    props: {
+      client: client.data,
+    },
+  };
+};
 
 export default ClientEditPage;
