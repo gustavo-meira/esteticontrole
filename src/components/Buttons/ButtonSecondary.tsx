c9aa8e17972bc391677b604b93caecb5aaec574d
Gustavo Meira <gmeira.antonio@gmail.com>
2023-02-08 16:50:29 -0300

    build: create a component to be a secondary button

---
+++ src/components/Buttons/ButtonSecondary.tsx
@@ -0,0 +1,16 @@
+import { Button, ButtonProps } from '@chakra-ui/react';
+
+export const ButtonSecondary = ({ children, ...rest }: ButtonProps) => (
+  <Button
+    type="button"
+    variant="solid"
+    border="2px solid #A87BC7"
+    bgColor="#FFFFFF"
+    color="#A87BC7"
+    colorScheme="whiteAlpha"
+    fontFamily="Poppins"
+    {...rest}
+  >
+    {children}
+  </Button>
+);
