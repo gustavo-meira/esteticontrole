19074568dba269c34b723f5b674719f21203a738
Gustavo Meira <gmeira.antonio@gmail.com>
2023-02-09 00:50:47 -0300

    style<TabClient>: styling the heading and the button to edit the client

--- src/components/Tabs/TabClient.tsx
+++ src/components/Tabs/TabClient.tsx
@@ -1,5 +1,7 @@
-import { Heading, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import { Heading, Tab, TabList, TabPanel, TabPanels, Tabs, Link, Box } from '@chakra-ui/react';
 import { Client, Measures, Package } from '@prisma/client';
+import { Pencil } from 'phosphor-react';
+import { ButtonPrimary } from '../Buttons/ButtonPrimary';
 import { PanelClient } from '../Panels/PanelClient';
 import { PanelMeasures } from '../Panels/PanelMeasures';
 import { TableClientPackages } from '../Tables/TableClientPackages';
@@ -13,10 +15,18 @@ type TabClientProps = {
 
 export const TabClient = ({ client }: TabClientProps) => (
   <>
-    <Heading textAlign="center" m="2">
-      {client.name}
-    </Heading>
-    <Tabs>
+    <Box position="relative">
+      <Heading fontWeight="normal" textAlign="center" m="2">
+        {client.name}
+      </Heading>
+      <Link width="fit-content" href={`/client/${client.id}/edit`}>
+        <ButtonPrimary right="12" bottom="-4" position="absolute" display="flex" gap="2">
+          <Pencil />
+          Editar
+        </ButtonPrimary>
+      </Link>
+    </Box>
+    <Tabs textColor="#635C66">
       <TabList>
         <Tab>Informações gerais</Tab>
         <Tab>Medidas</Tab>
