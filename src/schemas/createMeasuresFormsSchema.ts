55c6e2285fb76caf661ccca0747883285bfad6f3
Gustavo Meira <gmeira.antonio@gmail.com>
2023-02-11 19:47:20 -0300

    build<schemas>: taking off the measures from the client and create it's own schema

---
+++ src/schemas/createMeasuresFormsSchema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const createMeasuresSchema = z.object({
+  rightArm: z.string().optional(),
+  leftArm: z.string().optional(),
+  chest: z.string().optional(),
+  waist: z.string().optional(),
+  hips: z.string().optional(),
+  butt: z.string().optional(),
+  rightThigh: z.string().optional(),
+  leftThigh: z.string().optional(),
+  rightCalf: z.string().optional(),
+  leftCalf: z.string().optional(),
+  height: z.string().optional(),
+  weight: z.string().optional(),
+});
