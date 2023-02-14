a407e213b368c8ef4dbee6bbe64023a46bf4246d
Gustavo Meira <gmeira.antonio@gmail.com>
2023-02-13 23:20:42 -0300

    fix<capitalizeEveryStartLetter>: putting a trim method if the strings end or initiate with a empty space

--- src/utils/capitalizeEveryStartLetter.ts
+++ src/utils/capitalizeEveryStartLetter.ts
@@ -1,5 +1,5 @@
 export const capitalizeEveryStartLetter = (text: string) => {
-  const textSplitted = text.split(' ');
+  const textSplitted = text.trim().split(' ');
   return textSplitted.reduce((acc, curr) => {
     const currFirstLetter = curr[0].toLocaleUpperCase();
     return `${acc} ${currFirstLetter}${curr.slice(1)}`.trim();
