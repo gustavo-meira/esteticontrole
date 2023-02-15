b301925bb70bff4dde173c84b1406d78a19526ff
Gustavo Meira <gmeira.antonio@gmail.com>
2023-02-15 19:48:56 -0300

    feat<Statistics Api>: create a route to get information about the packages by year

---
+++ src/pages/api/statistics/counter-packages-by-year.ts
@@ -0,0 +1,61 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { z, ZodError } from 'zod';
+import { prisma } from '../../../lib/prisma';
+
+const acceptedMethods = ['GET'];
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (!acceptedMethods.includes(req.method as string)) {
+    res.status(405).json({ message: 'Method not implemented.' });
+  }
+
+  if (req.method === 'GET') {
+    const mostOldYear = 2015;
+    const mostRecentYear = new Date().getFullYear();
+
+    const possibleYears = Array.from({ length: mostRecentYear - mostOldYear + 1 }, (_, i) => i + mostOldYear);
+
+    if (!req.query.year) {
+      res.status(200).json({ possibleYears });
+    } else {
+      const queryYearSchema = z
+        .string()
+        .regex(/^\d+$/, { message: 'year must be a numeric string' })
+        .transform((value) => Number(value))
+        .refine((value) => possibleYears.includes(value), { message: 'year must be in possible years' });
+
+      try {
+        const year = queryYearSchema.parse(req.query.year);
+
+        const packagesOfTheYear = await Promise.all(
+          Array.from({ length: 12 }, (_, i) => i + 1).map(async (month) => {
+            const yearMaxToSearch = month + 1 > 12 ? year + 1 : year;
+            const monthMaxToSearch = month + 1 > 12 ? 1 : month + 1;
+
+            const packages = await prisma.package.count({
+              where: {
+                date: {
+                  gte: new Date(`${year}-${month}-01`),
+                  lt: new Date(`${yearMaxToSearch}-${monthMaxToSearch}-01`),
+                },
+              },
+            });
+            
+            return packages;
+          })
+        );
+
+        res.status(200).json({ packagesOfTheYear });
+      } catch (err) {
+        if (err instanceof ZodError) {
+          return res.status(400).json(err.message);
+        }
+
+        res.status(500).end();
+      }
+
+    }
+  }
+};
+
+export default handler;
