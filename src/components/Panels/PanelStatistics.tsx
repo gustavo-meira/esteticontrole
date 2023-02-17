c51358f71547efccb7a790c2c91ab3ee8c9c955f
Gustavo Meira <gmeira.antonio@gmail.com>
2023-02-16 22:56:30 -0300

    style<PanelStatistics>: styling the statistics to use all page

--- src/components/Panels/PanelStatistics.tsx
+++ src/components/Panels/PanelStatistics.tsx
@@ -1,4 +1,6 @@
+import { Flex } from '@chakra-ui/react';
 import { StatisticsCounterResponse } from '../../types/statistics.server';
+import { ChartClientsPackages } from '../Charts/ChartClientsPackages';
 import { InfoClientsPackagesRegistered } from '../Miscellaneous/InfoClientsPackagesRegistered';
 
 type PanelStatisticsProps = {
@@ -6,10 +8,11 @@ type PanelStatisticsProps = {
 };
 
 export const PanelStatistics = ({ statistics }: PanelStatisticsProps) => (
-  <div>
+  <Flex grow="1" justifyContent="space-around">
+    <ChartClientsPackages />
     <InfoClientsPackagesRegistered
       numberOfClients={statistics.numberOfClients}
       numberOfPackages={statistics.numberOfPackages}
     />
-  </div>
+  </Flex>
 );
