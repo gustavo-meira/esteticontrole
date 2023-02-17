4cf978fb4474fc70cd99a5310f98443a9934b01b
Gustavo Meira <gmeira.antonio@gmail.com>
2023-02-16 22:56:01 -0300

    style<InfoClientsPackagesRegistered>: styling the component

--- src/components/Miscellaneous/InfoClientsPackagesRegistered.tsx
+++ src/components/Miscellaneous/InfoClientsPackagesRegistered.tsx
@@ -7,18 +7,23 @@ type InfoClientsPackagesRegisteredProps = {
 };
 
 export const InfoClientsPackagesRegistered = ({ numberOfClients, numberOfPackages }: InfoClientsPackagesRegisteredProps) => (
-  <>
-    <Flex flexDir="column">
-      <CountUp
-        duration={4}
-        end={numberOfClients}
-        useEasing
-      />
-      <Text>
-        Clientes cadastrados
-      </Text>
-    </Flex>
-    <Flex flexDir="column">
+  <Flex
+    mt="12"
+    justifyContent="space-evenly"
+    alignItems="center"
+    flexDir="column"
+    minW="400px"
+    maxH="500px"
+  >
+    <Flex 
+      gap="2"
+      flexDir="column"
+      textAlign="center"
+      border="2px solid #635C66"
+      borderRadius="md"
+      padding="4"
+      maxW="40"
+    >
       <CountUp
         duration={4}
         end={numberOfPackages}
@@ -28,5 +33,27 @@ export const InfoClientsPackagesRegistered = ({ numberOfClients, numberOfPackage
         Pacotes cadastrados
       </Text>
     </Flex>
-  </>
+    <Flex 
+      gap="2"
+      flexDir="column"
+      textAlign="center"
+      border="2px solid #635C66"
+      borderRadius="md"
+      padding="4"
+      maxW="40"
+    >
+      <Text
+        fontSize="xl"
+      >
+        <CountUp
+          duration={4}
+          end={numberOfClients}
+          useEasing
+        />
+      </Text>
+      <Text>
+        Clientes cadastrados
+      </Text>
+    </Flex>
+  </Flex>
 );
