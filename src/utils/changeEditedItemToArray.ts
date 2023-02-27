84d417e59e87d4f05e226c058e8376224f74517e
Gustavo Meira <gmeira.antonio@gmail.com>
2023-02-27 15:02:28 -0300

    refactor<changeEditedItemToArray>:  instead of package use item

--- src/utils/changeEditedItemToArray.ts
+++ src/utils/changeEditedItemToArray.ts
@@ -2,9 +2,9 @@ type IdString = {
   id: string;
 };
 
-export const changeEditedItemToArray = <Item>(items: (Item & IdString)[], editedPackage: (Item & IdString)): Item[] => {
-  const indexEditedItem = items.findIndex(({ id }) => id === editedPackage.id);
+export const changeEditedItemToArray = <Item>(items: (Item & IdString)[], editedItem: (Item & IdString)): Item[] => {
+  const indexEditedItem = items.findIndex(({ id }) => id === editedItem.id);
   const copyItems = [...items];
-  copyItems[indexEditedItem] = editedPackage;
+  copyItems[indexEditedItem] = editedItem;
   return copyItems;
 };
