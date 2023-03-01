1552fe5ccff40972a584968abe429a5df9ac5113
Gustavo Meira <gmeira.antonio@gmail.com>
2023-02-28 23:31:51 -0300

    refactor<CliendEditPage>: the page now uses client services api

--- src/pages/client/[id]/edit.tsx
+++ src/pages/client/[id]/edit.tsx
@@ -1,12 +1,12 @@
-import { Client, Measures } from '@prisma/client';
 import { GetServerSideProps } from 'next';
 import { ContainerBasic } from '../../../components/Containers/ContainerBasic';
 import { FormsCreateClient } from '../../../components/Forms/FormsCreateClient';
 import { Header } from '../../../components/Miscellaneous/Header';
-import { api } from '../../../lib/api';
+import { Client } from '@prisma/client';
+import clientService from '../../../services/client';
 
 type ClientEditPageProps = {
-  client: Client & { measures: Measures };
+  client: Client;
 };
 
 const ClientEditPage = ({ client }: ClientEditPageProps) => (
@@ -17,14 +17,21 @@ const ClientEditPage = ({ client }: ClientEditPageProps) => (
 );
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const id = ctx.query.id as string;
-  const host = process.env.HOST as string;
+  const id = ctx.query.id;
 
-  const client = await api.get<Client & { measures: Measures }>(`${host}/api/client/${id}`);
+  if (typeof id !== 'string' || !id) {
+    return {
+      props: {
+        client: null,
+      },
+    };
+  }
+
+  const client = await clientService.getOne(id);
 
   return {
     props: {
-      client: client.data,
+      client,
     },
   };
 };
