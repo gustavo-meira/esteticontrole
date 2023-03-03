2a4b002337180a1a674671ff838ba96677715601
Gustavo Meira <gmeira.antonio@gmail.com>
2023-03-03 02:21:01 -0300

    feat<SchedulePage>: create the schedule page

---
+++ src/pages/schedule.tsx
@@ -0,0 +1,10 @@
+import { ContainerBasic } from '../components/Containers/ContainerBasic';
+import { Header } from '../components/Miscellaneous/Header';
+
+const SchedulePage = () => (
+  <ContainerBasic>
+    <Header />
+  </ContainerBasic>
+);
+
+export default SchedulePage;
