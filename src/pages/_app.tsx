e9953fe8db0dfac5e734b9d618d1795260810863
Gustavo Meira <gmeira.antonio@gmail.com>
2023-03-07 20:06:15 -0300

    build<AppComponent>: add the query client of react query to the app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import type { AppProps } from 'next/app';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/fonts.css';
+import { useState } from 'react';
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <ChakraProvider>
-    <Component {...pageProps} />
-  </ChakraProvider>
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Hydrate>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
