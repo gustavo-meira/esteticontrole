ca376ebf6a57ee3ee14d02b21749b1ccd8c20323
Gustavo Meira <gmeira.antonio@gmail.com>
2023-03-09 00:27:50 -0300

    feat<ListSchedulesOfADay>: after clicking on a schedule shows up the schedule infos

--- src/components/Lists/ListSchedulesOfADay.tsx
+++ src/components/Lists/ListSchedulesOfADay.tsx
@@ -1,4 +1,8 @@
+import { useDisclosure } from '@chakra-ui/react';
 import { Schedule } from '@prisma/client';
+import { useEffect, useState } from 'react';
+import { getHourAndMinutesFromDate } from '../../utils/getHourAndMinutesFromDate';
+import { AlertEditSchedule } from '../Alerts/AlertEditSchedule';
 
 type ListSchedulesOfADayProps = {
   date: Date;
@@ -15,16 +19,47 @@ const daysOfWeek = [
   'Sábado',
 ];
 
-export const ListSchedulesOfADay = ({ schedules, date }: ListSchedulesOfADayProps) => (
-  <div>
-    <h2>{daysOfWeek[date.getDay()]} | {date.toLocaleDateString('pt-BR')}</h2>
-    {
-      schedules.map((schedule) => (
-        <div key={schedule.id}>
-          <h3>{schedule.clientName}</h3>
-          <p>{new Date(schedule.startDate).toLocaleDateString('pt-BR')}</p>
-        </div>
-      ))
-    }
-  </div>
-);
+export const ListSchedulesOfADay = ({ schedules, date }: ListSchedulesOfADayProps) => {
+  const [scheduleToEdit, setScheduleToEdit] = useState<null | Schedule>(null);
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  useEffect(() => {
+    if (scheduleToEdit) onOpen();
+  }, [scheduleToEdit]);
+
+  const onCloseAlert = () => {
+    setScheduleToEdit(null);
+    onClose();
+  };
+
+  const listDay = `${daysOfWeek[date.getDay()]} | ${date.toLocaleDateString('pt-BR')}`;
+
+  return (
+    <div>
+      <h2>{listDay}</h2>
+      {
+        schedules.map((schedule) => {
+          const scheduleStartHour = getHourAndMinutesFromDate(schedule.startDate);
+          const scheduleEndHour = getHourAndMinutesFromDate(schedule.endDate);
+
+          return (
+            <button onClick={() => setScheduleToEdit(schedule)} key={schedule.id}>
+              <h3>{schedule.clientName}</h3>
+              <p>{`${scheduleStartHour} ~ ${scheduleEndHour}`}</p>
+              <p>{schedule.treatment || 'Tratamento não definido'}</p>
+            </button>
+          );})
+      }
+      { isOpen &&
+        scheduleToEdit &&
+        (
+          <AlertEditSchedule
+            schedule={scheduleToEdit}
+            isOpen={isOpen}
+            onClose={onCloseAlert}
+          />
+        )
+      }
+    </div>
+  );
+};
